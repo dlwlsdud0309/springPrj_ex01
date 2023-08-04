16c1059fac820ddbfd6bc32c959b762adce07a0d
dlwlsdud9439 <dlwlsdud9439@gmail.com>
2023-08-04 11:29:05 +0900

    @Log4j 어노테이션 에러 확인

--- src/main/java/org/zerock/controller/SampleController.java
+++ src/main/java/org/zerock/controller/SampleController.java
@@ -3,8 +3,17 @@ package org.zerock.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller
+import lombok.extern.log4j.Log4j;
+
+@Controller //스프링의 객체로 등록
+//servlet-context에 component-scan에서 controller 패키지 안의 어노테이션 @Controller가 추가된
+//클래스들을 객체로 생성하여 관리하게함
 @RequestMapping("/sample/*")
+@Log4j
 public class SampleController {
 
+	@RequestMapping("")
+	public void basic() {
+		
+	}
 }
