8b691fe19b084765092e3a42b53fed4411e03cad
dlwlsdud9439 <dlwlsdud9439@gmail.com>
2023-08-09 17:13:07 +0900

    @RequestParam은 파라미터로 사용된 변수의 이름과 전달되는 파라미터의 이름이 다른 경우에 사용한다, 동일한 이름의 파라미터가 여러개 전달되는 경우 @RequestParam 어노테이션과 ArrayList<> 혹은 배열을 사용하여 처리한다

--- src/main/java/org/zerock/controller/SampleController.java
+++ src/main/java/org/zerock/controller/SampleController.java
@@ -1,9 +1,13 @@
 package org.zerock.controller;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.zerock.domain.SampleDTO;
 
 import lombok.extern.log4j.Log4j;
@@ -36,6 +40,32 @@ public class SampleController {
 		log.info(""+dto);
 		
 		return "ex01";
+	}
+	
+	@GetMapping("/ex02")
+	public String ex02(@RequestParam("name") String name, @RequestParam("age") int age) {
+		
+		log.info("name : "+name);
+		log.info("age : "+age);
+		
+		return "ex02";
+	}
+	
+	//동일한 이름의 파라미터가 여러 개 전달되는 경우
+	@GetMapping("/ex02List")
+	public String ex02List(@RequestParam("ids")ArrayList<String> ids) {
+		
+		log.info("ids : "+ids);
+		
+		return "ex02List";
+	}
+	
+	@GetMapping("/ex02Array")
+	public String ex02Array(@RequestParam("ids") String[] ids) {
+		
+		log.info("array ids : "+Arrays.toString(ids));
+		
+		return "ex02Array";
 	}
 	
 }
