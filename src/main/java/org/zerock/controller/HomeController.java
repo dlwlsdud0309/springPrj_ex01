b7c41934ec6eac48ac77c3c31564f5ce7178c987
dlwlsdud9439 <dlwlsdud9439@gmail.com>
2023-08-12 11:36:05 +0900

    @RequestMapping(value = "/", method = RequestMethod.GET)와 @GetMapping 차이

--- src/main/java/org/zerock/controller/HomeController.java
+++ src/main/java/org/zerock/controller/HomeController.java
@@ -22,6 +22,17 @@ public class HomeController {
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
+	
+	//@GetMapping / @PostMapping
+	//요청방식별로 제공되는 어노테이션
+	
+	//@RequestMapping
+	//URL을 컨트롤러의 메서드와 매핑할 때 사용하는 어노테이션
+	//요청 주소(url) 설정, 요청 방식(GET, POST, DELETE, PATCH) 설정
+	//요청방식들을 동시에 설정 가능
+	//@RequestMapping(value = "/test", method = {RequestMethod.GET, RequestMethod.POST})
+	
+	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
 		logger.info("Welcome home! The client locale is {}.", locale);
